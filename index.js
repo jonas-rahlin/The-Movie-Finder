be866c73493173210410f1425f4a35ffe8b240d4
Leafhill <69792376+jonas-rahlin@users.noreply.github.com>
2021-01-28 17:28:09 +0100

    New Commit
    Added Comments to JS and some more CSS styling for the search input.
    
    Removed focus effects and search clear form search input and search button.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+//Search Button <button>(id="search-btn")
 const searchBtn = document.getElementById("search-btn");
+
+//Movie container <ol>(id="movie-list")
 const movieList = document.getElementById("movie-list");
+
+//Empty array for filling with information retrieved from API
 let movies = [];
 
-
+//When search-button is clicked:
 searchBtn.addEventListener("click", async () => {
     let movies = [];
     movieList.innerHTML = "";
@@ -10,6 +15,17 @@ searchBtn.addEventListener("click", async () => {
     displayMovies();
 })
 
+//Get the list of movies from the API based on the search string inputed into: <input>(id="search-input").
+const getMovies = async () => {
+    const searchInput = document.getElementById("search-input").value;
+    const apiCall = `http://www.omdbapi.com/?apikey=9c1e556d&s=${searchInput}`;
+    const res = await fetch(apiCall);
+    const data = await res.json();
+    console.log(data.Search);
+    movies = data.Search;
+}
+
+//Create a new list item for each movie in the array. Then add those list items to <ol>(id="movie-list")
 const displayMovies = () => {
     for(let i = 0; i < movies.length; i++){
         const movie = document.createElement("li");
@@ -21,13 +37,4 @@ const displayMovies = () => {
         <a class="imdb" target="_blank" href="https://www.imdb.com/title/${movies[i].imdbID}/">IMDB</a>`
         movieList.append(movie);
     }
-}
-
-const getMovies = async () => {
-    const searchInput = document.getElementById("search-input").value;
-    const apiCall = `http://www.omdbapi.com/?apikey=9c1e556d&s=${searchInput}`;
-    const res = await fetch(apiCall);
-    const data = await res.json();
-    console.log(data.Search);
-    movies = data.Search;
 }
